bdee8486ba4f5334747dda481dd337a8bcecdea4
Sravani <sravani2044@gmail.com>
2014-11-11 17:22:08 +0530

    Update CustomGridViewAdapter.java

--- src/com/playforgood/ui/CustomGridViewAdapter.java
+++ src/com/playforgood/ui/CustomGridViewAdapter.java
@@ -13,11 +13,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-/**
- * 
- * @author manish.s
- *
- */
 public class CustomGridViewAdapter extends ArrayAdapter<Item> {
 	Context context;
 	int layoutResourceId;
